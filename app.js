be1d0fe00e7077c37e106de0531779f672ee6920
Ryu.ktym <226134443+rkworks2025-coder@users.noreply.github.com>
2025-08-30 09:34:48 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
-const GAS_URL="https://script.google.com/macros/s/AKfycby9NsfFHpjMI2GGQA2Jw8DcFuRs9aU74cuNWWvRWWe9SiWivhm6PtFmw5nCdsgxpTvLFw/exec";
-const App={};
-App.syncAll=()=>{alert("同期処理 202508292301")};
-App.renderArea=(c)=>{document.getElementById("list").innerHTML="<div>"+c+"リスト</div>";};
-App.initIndex=()=>{document.getElementById("btnSync").onclick=App.syncAll;};
+console.log('app.js loaded');
