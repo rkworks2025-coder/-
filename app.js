9902e1dabf88eeb46bca5512c7c41da1457969a1
Ryu.ktym <226134443+rkworks2025-coder@users.noreply.github.com>
2025-10-30 21:03:33 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // ====== 設定 ======
 const Junkai = (()=>{
 
-  const GAS_URL = "https://script.google.com/macros/s/AKfycbzCdDoldtJN_F2qB8aJoAwZinoVjunSq6Xf7qVdb11lB0-NLz4Dk2YfAN5iLb1Qdt8a/exec";
+  const GAS_URL = "https://script.google.com/macros/s/AKfycby9RpFiUbYu6YX6JW9XfwbDx36_4AIlGEQMOxR3SnxgNdoRUJKfyxvF3b1SEYwuHb3X/exec";
   const TIRE_APP_URL = "https://rkworks2025-coder.github.io/r.k.w-/";
   const CITIES = ["大和市","海老名市","調布市"];
   const PREFIX = { "大和市":"Y", "海老名市":"E", "調布市":"C" };
