3e478e5cb34c5a5a063ba536b710d4a85c54f676
Ryu.ktym <226134443+rkworks2025-coder@users.noreply.github.com>
2025-11-03 00:52:04 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // ====== 設定 ======
 const Junkai = (()=>{
 
-  const GAS_URL = "https://script.google.com/macros/s/AKfycby9RpFiUbYu6YX6JW9XfwbDx36_4AIlGEQMOxR3SnxgNdoRUJKfyxvF3b1SEYwuHb3X/exec";
+  const GAS_URL = "https://script.google.com/macros/s/AKfycbyXbPaarnD7mQa_rqm6mk-Os3XBH6C731aGxk7ecJC5U3XjtwfMkeF429rezkAo79jN/exec";
   const TIRE_APP_URL = "https://rkworks2025-coder.github.io/r.k.w-/";
   const CITIES = ["大和市","海老名市","調布市"];
   const PREFIX = { "大和市":"Y", "海老名市":"E", "調布市":"C" };
